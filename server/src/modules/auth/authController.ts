9f56a88126dc2497fe2af8192e1286b30dfc5b40
CristianmMst <cristianmmst@gmail.com>
2025-04-11 09:44:46 -0500

    feature: Login distributon & employee

--- server/src/modules/auth/authController.ts
+++ server/src/modules/auth/authController.ts
@@ -1,17 +1,59 @@
 import AuthService from "./authService";
 import { NextFunction, Request, Response } from "express";
+import EmployeeService from "../employees/employeeService";
 import DistributorService from "../distributors/distributorService";
 
+enum TYPE_USERS {
+  EMPLOYEE = "EMPLOYEE",
+  DISTRIBUTOR = "DISTRIBUTOR",
+}
+
 class AuthController {
-  private authService = new AuthService();
-  private distributorService = new DistributorService();
+  private readonly authService = new AuthService();
+  private readonly employeeService = new EmployeeService();
+  private readonly distributorService = new DistributorService();
 
   login = async (req: Request, res: Response, next: NextFunction) => {
-    const { id, name, email, password, location } = req.body;
-    console.log(id, name, email, password, location);
-
+    const { email, password } = req.body;
     try {
-      res.status(200).send({ message: "Login exitoso" });
+      const employee = await this.employeeService.findOneByEmail(email);
+      const distributor = await this.distributorService.findOneByEmail(email);
+      if (!distributor && !employee) {
+        throw new Error("Usuario no encontrado");
+      }
+
+      if (employee) {
+        const isPasswordCorrect = this.authService.comparePassword(
+          password,
+          employee.password,
+        );
+        if (!isPasswordCorrect) {
+          throw new Error("Contraseña incorrecta");
+        }
+        const token = this.authService.createToken(employee.id, email);
+        res.cookie("accessToken", token, {
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24 * 7,
+        });
+        res
+          .status(200)
+          .send({ token, type: TYPE_USERS.EMPLOYEE, role: employee.role });
+      }
+      if (distributor) {
+        const isPasswordCorrect = this.authService.comparePassword(
+          password,
+          distributor.password,
+        );
+        if (!isPasswordCorrect) {
+          throw new Error("Contraseña incorrecta");
+        }
+        const token = this.authService.createToken(distributor.id, email);
+        res.cookie("accessToken", token, {
+          httpOnly: true,
+          maxAge: 1000 * 60 * 60 * 24 * 7,
+        });
+        res.status(200).send({ token, type: TYPE_USERS.DISTRIBUTOR });
+      }
     } catch (error) {
       next(error);
     }
