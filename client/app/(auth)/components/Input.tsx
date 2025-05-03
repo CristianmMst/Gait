c3ccf1fb22362e862f676cb9db9d9d12f77bbe79
CristianmMst <cristianmmst@gmail.com>
2025-05-03 13:49:39 -0500

    add:className attribute to component Input

--- client/app/(auth)/components/Input.tsx
+++ client/app/(auth)/components/Input.tsx
@@ -1,15 +1,21 @@
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
   error?: string[];
+  className?: string;
 }
 
-export default function Input({ label, error, ...props }: InputProps) {
+export default function Input({
+  label,
+  error,
+  className,
+  ...props
+}: InputProps) {
   return (
     <label className="flex flex-col mb-2">
       <span className="text-sm mb-2">{label}</span>
       <input
         {...props}
-        className={`bg-zinc-800 border rounded-md p-2 placeholder:text-sm ${!!error?.length ? "border-red-600" : "border-zinc-700"}`}
+        className={`bg-zinc-800 border rounded-md p-2 placeholder:text-sm ${!!error?.length ? "border-red-600" : "border-zinc-700"} ${className ? className : ""}`}
       />
       {error && <p className="text-xs mt-2 ml-1 text-red-600">{error}</p>}
     </label>
