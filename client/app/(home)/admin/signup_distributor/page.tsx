da31f12919f81a7ea24314576b92b2b441388542
CristianmMst <cristianmmst@gmail.com>
2025-09-30 18:55:30 -0500

    style: improve layout and styling of SignupDistributor component

--- client/app/(home)/admin/signup_distributor/page.tsx
+++ client/app/(home)/admin/signup_distributor/page.tsx
@@ -27,11 +27,11 @@ export default function SignupDistributor() {
 
   return (
     <div className="grid place-content-center h-screen">
-      <h1 className="text-xl font-bold">Registra un distribuidor</h1>
-      <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-2 bg-zinc-950 border border-zinc-900 p-6 rounded-md ">
+        <h2 className="text-xl font-bold">Registra un distribuidor</h2>
         <p>Genera un link para que un distribuidor pueda registrarse</p>
-        <div className="flex flex-col gap-y-4">
-          <div className="flex justify-between bg-zinc-800 w-140 h-10 rounded-sm px-4 gap-4">
+        <div className="flex flex-col gap-y-4 mt-2">
+          <div className="flex justify-between bg-zinc-950 border border-zinc-900 w-140 h-10 rounded-sm px-4 gap-4">
             <div className="flex items-center gap-4 w-full">
               <Link size={20} />
               <input
@@ -51,7 +51,7 @@ export default function SignupDistributor() {
           </div>
           <button
             onClick={handleGenerate}
-            className="flex justify-center items-center bg-accent text-black p-2 rounded-sm font-bold gap-2 active:scale-95 transition-transform hover:opacity-90"
+            className="flex justify-center items-center bg-accent text-black p-2 rounded-sm font-bold gap-2 active:scale-95 transition-transform hover:opacity-90 cursor-pointer"
           >
             <RotateCcw size={20} />
             <span>{isGenerated ? "Generado" : "Generar"}</span>
