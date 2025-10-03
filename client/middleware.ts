593aee6d7a39acede1492fa9a02a4c716a8acf3f
CristianmMst <cristianmmst@gmail.com>
2025-10-03 18:38:33 -0500

    feat: update middleware to include additional protected routes and refine admin route handling

--- client/middleware.ts
+++ client/middleware.ts
@@ -2,19 +2,28 @@ import { TYPE_USERS } from "./app/shared/enums/user";
 import { NextRequest, NextResponse } from "next/server";
 import { verifySession } from "./app/(auth)/actions/verifySession";
 
-const protectedRoutes = ["/"];
+const protectedRoutes = ["/", "/orders", "/shop", "/distributor"];
 
-const adminRoutes = ["/admin", "/admin/signup_distributor"];
+const adminRoutes = ["/admin"];
 
 const publicRoutes = ["/login", "/signup", "/admin/login"];
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isPublic = publicRoutes.includes(path);
-  const isProtected = protectedRoutes.includes(path);
   const isAdminRoute =
     path !== "/admin/login" &&
     adminRoutes.some((route) => path.startsWith(route));
+
+  const isProtected =
+    !isPublic &&
+    !isAdminRoute &&
+    protectedRoutes.some((route) => {
+      if (route === "/") {
+        return path === "/";
+      }
+      return path.startsWith(route);
+    });
 
   const session = await verifySession();
 
